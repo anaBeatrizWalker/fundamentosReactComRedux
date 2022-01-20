6ab83332e2eeaf368326d4e2f0b4bac168439719
Ana Beatriz <anabiawalker@gmail.com>
2022-01-20 17:32:54 -0300

    Usando componentes filhos

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Segundo, { Primeiro } from './component'
+import SilvaFamily from './silvaFamily'
 
 ReactDOM.render(
-    //mais de um componente = precisa ser agrupado em uma div
     <div>
-        <Primeiro/>
-        <Segundo/>
+        <SilvaFamily/>
     </div>
     , document.getElementById('app')
 )
---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Members from "./members"
+
+export default props => (
+    <div>
+        <Members name='João' lastName='Silva'/>
+        <Members name='Maria' lastName='Silva'/>
+        <Members name='José' lastName='Silva'/>
+        <Members name='Pedro' lastName='Silva'/>
+    </div>
+)

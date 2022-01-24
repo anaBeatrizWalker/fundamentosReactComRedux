5f0acbe4f573b936d7530cd63e57cbd8d7d4fd4e
Ana Beatriz <anabiawalker@gmail.com>
2022-01-24 14:50:25 -0300

    Adicionando e Configurando o Redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import Field from './field'
 
+const reducers = combineReducers({
+    field: () => ({value: 'Teste'}) //estado fixo
+})
+
 ReactDOM.render(
-    <Field initialValue='Mude o label'/>
+    //criando store (objeto com todos os reducers)
+    <Provider store={createStore(reducers)}> 
+        <Field initialValue='Mude o label'/>
+    </Provider>
     , document.getElementById('app')
 )
